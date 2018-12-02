4c56f6e1140ba35bd305effbcb757aaa38aa91f3
Iliyan Iliev <iliyan94iliev@gmail.com>
2018-12-02 21:59:19 +0200

    vue added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import css from './style.css';
+import Vue from 'vue';
+
+Vue.component('todo-item', {
+  template: '<li>This is a todo</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
