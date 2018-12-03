9a846335a24b6956c6f8f765c3ab89742b97498e
Iliyan Iliev <i.iliev@sepa-cyber.com>
2018-12-03 10:38:01 +0200

    vue-components config added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import css from './style.css';
 import Vue from 'vue';
+import dashboard from './dashboard.vue';
 
-Vue.component('todo-item', {
-  template: '<li>This is a todo</li>'
-})
+Vue.component('dashboard', dashboard)
 
 var app7 = new Vue({
   el: '#app-7',
@@ -14,4 +13,4 @@ var app7 = new Vue({
       { id: 2, text: 'Whatever else humans are supposed to eat' }
     ]
   }
-})
+});
